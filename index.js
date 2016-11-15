7f35e87b428788b0e80af805bab1afae6c26b8df
NerveClasp <nerveclasp@gmail.com>
2016-11-15 05:57:24 +0200

    A few first bugs suposedly fixed. Needs moar testing

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const moment = require('moment');
 
 let countMe = 0;
 let config = require('./config.json');
-console.log(countMe);
-console.log(config.consumerKey);
-console.log(moment().format('HH:mm'));
 
 var client = new twitter({
   consumer_key: config.consumerKey,
@@ -16,17 +13,18 @@ var client = new twitter({
   access_token_secret: config.accessTokenSecret
 });
 
-let time = moment().format('HH:mm');
-
 setInterval(function () {
+  let time = moment().format('HH:mm');
   if(time[0] == time[4] && time[1] == time[3]){
-    client.post('statuses/update', {status: tweets[countMe]+'\n'+time},  function(error, tweet, response) {
+  // if(true){
+    client.post('statuses/update', {status: tweets.t[countMe]+'\n'+time},  function(error, tweet, response) {
       if(error){
         /* lol nothing */
       }else{
         countMe++;
       }
     });
+  // }//
   }else{
     console.log(moment().format("HH:mm:ss")+" - no tweeting required at this time.");
   }
