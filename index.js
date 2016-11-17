20fcd42924035a1820aa8f814d5c60715980abb1
NerveClasp <nerveclasp@gmail.com>
2016-11-17 09:47:40 +0200

    Added a messages buffer functionality for cases when noone will use the tag to add new tweets to the database and if I will forget to add new ones by myself. Once there are no available messages, it will tweet four messages with my username in it. Once those are done it will start posting again from the start. Avoiding "undefined" tweets in such a rough manner:)

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const twitter = require('twitter');
 const tweets = require('./testPhrases.json');
 const moment = require('moment');
 
-let countMe = 0;
+let countMe, bufferCount = 0;
 let config = require('./config.json');
+let tweet = "";
+let bufferTweets = [
+  "твітнути щось з тегом #perfect_time_ukr ібо тут вже твіти вигадані @NerveClasp закінчуються!",
+  "терміново твітнути щось з тегом #perfect_time_ukr бо @NerveClasp забув додати тупих фразочок..",
+  "подзвонити @NerveClasp аби додав ще фразочок, бо старі закінчились зовсім.. #perfect_time_ukr",
+  "всі про мене забули :,( Ну знач наша пісня гарна нóва, починаймо її знову, пра @NerveClasp ?"
+];
 
 var client = new twitter({
   consumer_key: config.consumerKey,
@@ -15,17 +22,26 @@ var client = new twitter({
 
 setInterval(function () {
   let time = moment().format('HH:mm');
+  if(tweets.t.length == countMe || tweets.t.length < countMe){
+    tweet = bufferTweets[bufferCount];
+    bufferCount++;
+  }else if(tweets.t.length < countMe && bufferCount == bufferTweets.length){
+    countMe = 0;
+    bufferCount = 0;
+    tweet = tweets.t[countMe];
+  }else{
+    tweet = tweets.t[countMe];
+  }
   if(time[0] == time[4] && time[1] == time[3]){
   // if(true){
-    client.post('statuses/update', {status: tweets.t[countMe]+'\n'+time},  function(error, tweet, response) {
+    client.post('statuses/update', {status: tweet+'\n'+time},  function(error, tweet, response) {
       if(error){
         /* lol nothing */
       }else{
+        console.log(moment().format("HH:mm:ss_")+"tweeted"+tweets.t[countMe]);
         countMe++;
       }
     });
   // }//
-  }else{
-    console.log(moment().format("HH:mm:ss")+" - no tweeting required at this time.");
   }
 }, 60000);
